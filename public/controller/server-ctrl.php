22b75f7ff1ef28bc62f4497cdf143d9c48e6e69f
Quentin Warth <qwarth@arhcimed-ge.com>
2023-03-10 14:47:15 +0100

    retirage vérif

--- public/controller/server-ctrl.php
+++ public/controller/server-ctrl.php
@@ -2,21 +2,20 @@
 
 $match = $router->match();
 
-echo "server<br>";
-echo "$match<br>";
+// echo "server<br>";
+// echo "$match<br>";
 
 if (is_array($match)) {
-    echo "$match";
+    // echo "$match";
     if (is_callable($match['target'])) {
         call_user_func_array($match['target'], $match['params']);
     } else {
         $params = $match['params'];
 
-        echo "$params";
+        // echo "$params";
 
         require "../server/{$match['target']}.php";
     }
-    require "../server/{$match['target']}.php";
 } else {
     throw new Exception('Error 404:Server Error');
 }
