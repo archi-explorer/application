e0869143a3ef8fa4395d6b339d65f668c764c734
Quentin Warth <qwarth@arhcimed-ge.com>
2023-03-10 12:00:06 +0100

    maj vérif

--- public/controller/server-ctrl.php
+++ public/controller/server-ctrl.php
@@ -3,13 +3,13 @@
 $match = $router->match();
 
 if (is_array($match)) {
-    echo $match;
+    echo "$match";
     if (is_callable($match['target'])) {
         call_user_func_array($match['target'], $match['params']);
     } else {
         $params = $match['params'];
 
-        echo $params;
+        echo "$params";
 
         require "../server/{$match['target']}.php";
     }
