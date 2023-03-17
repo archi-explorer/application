4b23339badd68d016aa59218bd153b265f14f6b9
Quentin Warth <qwarth@arhcimed-ge.com>
2023-03-17 15:25:34 +0100

    update route

--- public/routes/routes.php
+++ public/routes/routes.php
@@ -38,4 +38,4 @@ $router->map('PUT', '/update-model', 'server/model/update-model');
 $router->map('DELETE', '/delete-model', 'server/model/delete-model');
 
 
-require "../controller/client-ctrl.php";
+require "../controller/api-ctrl.php";
